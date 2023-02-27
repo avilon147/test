65d109aaca79e09499dcf20f1d5bcf86747a3f10
avilon <avilon147@gmail.com>
2023-02-27 16:30:52 +0700

    test

---
+++ #include <cstdio>.c
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+int main() {
+  int age = 23;
+
+  // print a string literal 
+  printf("My age is ");
+
+
+  // print an int variable
+  printf("%d", age);
+
+    
+  return 0;
+}
+// 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
+
+// Output: My age is 23
